ef8da68bd129b789929ff1bcd0564a27526514ae
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-06-03 16:16:39 +0530

    S3 service, reading all data inside a bucket

--- file-storage/S3Writter.java
+++ file-storage/S3Writter.java
@@ -20,7 +20,7 @@ public class S3Writter {
 
         AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(Regions.us-east-1)
+                .withRegion(Regions.us - east - 1)
                 .build();
 
         // Create a bucket
@@ -37,5 +37,27 @@ public class S3Writter {
 
         // Delete the bucket
         s3Client.deleteBucket(bucketName);
+
+        // List all objects in the bucket
+        ListObjectsV2Request listObjectsRequest = new ListObjectsV2Request()
+                .withBucketName(bucketName)
+                .withMaxKeys(1000); // Maximum number of keys to retrieve in one request
+
+        ListObjectsV2Result result;
+
+        do {
+            result = s3Client.listObjectsV2(listObjectsRequest);
+
+            for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
+                // Access the details of each object
+                String objectKey = objectSummary.getKey();
+                long size = objectSummary.getSize();
+                System.out.println("Key: " + objectKey + ", Size: " + size);
+            }
+
+            // Set the next continuation token to retrieve the next set of keys
+            String token = result.getNextContinuationToken();
+            listObjectsRequest.setContinuationToken(token);
+        } while (result.isTruncated());
     }
 }
